a9061a5a0502ba2332218185b2b571a4145154a8
Alexandre Capt <acapt@adobe.com>
2019-08-26 14:46:25 +0200

    Fix linting

--- cypress/integration/navigation.spec.js
+++
@@ -1,8 +0,0 @@
-/// <reference types="Cypress" />
-
-context('Navigation', () => {
-  it('home', () => {
-    cy.visit('http://localhost:3000')
-    cy.title().should('include', 'Helix Pages')
-  })
-})
